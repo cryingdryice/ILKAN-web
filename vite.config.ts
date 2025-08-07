faafb9d75684f0db0fcc4517b37d8d0ef8c05966
Jiwon Park <parkjiwon0821@naver.com>
2025-08-07 18:56:03 +0900

    chore: config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    open: true, // 자동으로 브라우저 열기
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+});
