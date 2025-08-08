cd3e7bd09a4b76982dd15f26eb03132c60bd761e
nyeonseok <aragorn2358@naver.com>
2025-08-08 11:35:49 +0900

    feat: profile information 레이아웃 구현

--- src/pages/main/myPage.tsx
+++ src/pages/main/myPage.tsx
@@ -16,7 +16,34 @@ export default function MyPage() {
               </div>
               <div className={myPageStyle.imgAndContentDiv}>
                 <div className={myPageStyle.profileImg}></div>
-                <div className={myPageStyle.profileContent}>d</div>
+                <div className={myPageStyle.profileContent}>
+                  <div className={myPageStyle.nameDiv}>
+                    <span className={myPageStyle.name}>김 토토</span>
+                    <span className={myPageStyle.age}>(여자, 25)</span>
+                  </div>
+                  <div className={myPageStyle.profileInformation}>
+                    <div className={myPageStyle.phoneDiv}>
+                      <label>연락처</label>
+                      <span>010-0000-0000</span>
+                    </div>
+                    <div className={myPageStyle.emailDiv}>
+                      <label>이메일</label>
+                      <span>abc1234@naver.com</span>
+                    </div>
+                    <div className={myPageStyle.addressDiv}>
+                      <label>주소</label>
+                      <span>하늘시 구름동 뭉게뭉게 304호</span>
+                    </div>
+                    <div className={myPageStyle.resumeDiv}>
+                      <label>이력서</label>
+                      <span>pdf 올리기</span>
+                    </div>
+                    <div className={myPageStyle.portfolioDiv}>
+                      <label>포트폴리오</label>
+                      <span>링크 입력? pdf 올리기</span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
