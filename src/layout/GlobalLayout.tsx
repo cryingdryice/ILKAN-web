8e02d5fd2dd4e82b631f057e1f2b9915f028e2c3
nyeonseok <aragorn2358@naver.com>
2025-08-08 09:58:03 +0900

    feat: 디자인 유지를 위한 GlobalLayout.tsx 수정, 이후 병합 시 원래대로 바꿈

--- src/layout/GlobalLayout.tsx
+++ src/layout/GlobalLayout.tsx
@@ -2,9 +2,26 @@ import { Outlet } from "react-router-dom";
 
 export default function GlobalLayout() {
   return (
-    <div>
-      <h1>GlobalLayout</h1>
-      <Outlet />
+    <div style={{ width: "100vw" }}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div
+          style={{
+            backgroundColor: "red",
+            width: "200px",
+            height: "100vh",
+          }}
+        >
+          사이드바
+        </div>
+        <div style={{ flex: "1" }}>
+          <div
+            style={{ width: "100%", backgroundColor: "blue", height: "80px" }}
+          >
+            header
+          </div>
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
