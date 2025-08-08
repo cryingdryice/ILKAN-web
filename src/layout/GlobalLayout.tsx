709e8c4bb9c31496c30771565c98dbbf080db7cd
chanju <ckswn1244@gmail.com>
2025-08-08 17:53:15 +0900

    전체 레이아웃 구성 및 ILKAN 이미지 삽입

--- src/layout/GlobalLayout.tsx
+++ src/layout/GlobalLayout.tsx
@@ -1,10 +1,33 @@
 import { Outlet } from "react-router-dom";
+import styles from "../css/GlobalLayout.module.css";
+import ILKAN from "../assets/IL-KAN.png";
 
 export default function GlobalLayout() {
   return (
-    <div>
-      <h1>GlobalLayout</h1>
-      <Outlet />
+    <div className={styles.wrapper}>
+      <div className={styles.sidebar}>
+        <img src={ILKAN} alt="IL-KAN 로고" className={styles.logo} />
+        <div className={styles.MYPAGE}>
+          <button className={styles.Btn}></button>
+          <div className={styles.Font}>MY PAGE</div>
+        </div>
+
+        <div className={styles.Line}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="200"
+            height="2"
+            viewBox="0 0 200 2"
+            fill="none"
+          >
+            <path d="M0 1H200" stroke="#D9D9D9" />
+          </svg>
+        </div>
+      </div>
+
+      <div className={styles.content}>
+        <Outlet />
+      </div>
     </div>
   );
 }
