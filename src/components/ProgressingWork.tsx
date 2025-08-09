c7017a88fe988a321615ea082c5f5b289d683a76
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-08-09 12:46:44 +0900

    fix: 일정한 디자인 유지를 위해 profile.module.css 수정

--- src/components/ProgressingWork.tsx
+++ src/components/ProgressingWork.tsx
@@ -1,4 +1,5 @@
 import progressingWorkStyle from "../css/components/progressingWork.module.css";
+import checkImg from "../assets/check.png";
 
 export default function ProgressingWork() {
   return (
@@ -7,9 +8,19 @@ export default function ProgressingWork() {
         <span>지금 진행중인 의뢰가 2건 있어요!</span>
       </div>
       <div className={progressingWorkStyle.body}>
-        <div className={progressingWorkStyle.top}>
-          <div className={progressingWorkStyle.checkDiv}></div>
-          <div className={progressingWorkStyle.priceDiv}></div>
+        <div className={progressingWorkStyle.topDiv}>
+          <div className={progressingWorkStyle.checkAndTitleDiv}>
+            <div className={progressingWorkStyle.checkDiv}>
+              <img src={checkImg} />
+              <span>진행중</span>
+            </div>
+            <div className={progressingWorkStyle.titleDiv}>
+              <span>화장품 텍스쳐 상세 정보란 사진 외주 </span>
+            </div>
+          </div>
+          <div className={progressingWorkStyle.priceDiv}>
+            <span>500,000원</span>
+          </div>
         </div>
         <div className={progressingWorkStyle.contentDiv}></div>
       </div>
