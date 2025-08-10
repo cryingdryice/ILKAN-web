f64c2c954d74c3f2803a86c0884bea62c434fdef
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-08-10 22:20:50 +0900

    feat: inner추가

--- src/components/ProgressingWork.tsx
+++ src/components/ProgressingWork.tsx
@@ -8,21 +8,25 @@ export default function ProgressingWork() {
         <span>지금 진행중인 의뢰가 2건 있어요!</span>
       </div>
       <div className={progressingWorkStyle.body}>
-        <div className={progressingWorkStyle.topDiv}>
-          <div className={progressingWorkStyle.checkAndTitleDiv}>
-            <div className={progressingWorkStyle.checkDiv}>
-              <img src={checkImg} />
-              <span>진행중</span>
+        <div className={progressingWorkStyle.inner}>
+          <div className={progressingWorkStyle.topDiv}>
+            <div className={progressingWorkStyle.checkAndTitleDiv}>
+              <div className={progressingWorkStyle.checkDiv}>
+                <img src={checkImg} />
+                <span>진행중</span>
+              </div>
+              <div className={progressingWorkStyle.titleDiv}>
+                <span>화장품 텍스쳐 상세 정보란 사진 외주 </span>
+              </div>
             </div>
-            <div className={progressingWorkStyle.titleDiv}>
-              <span>화장품 텍스쳐 상세 정보란 사진 외주 </span>
+            <div className={progressingWorkStyle.priceDiv}>
+              <span>500,000원</span>
             </div>
           </div>
-          <div className={progressingWorkStyle.priceDiv}>
-            <span>500,000원</span>
+          <div className={progressingWorkStyle.contentDiv}>
+            <div className={progressingWorkStyle.progressingBarDiv}></div>
           </div>
         </div>
-        <div className={progressingWorkStyle.contentDiv}></div>
       </div>
     </div>
   );
