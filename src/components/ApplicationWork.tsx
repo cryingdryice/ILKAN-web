6618bc099a5ce1414f9bd6e0bc8e1758732e0c57
nyeonseok <aragorn2358@naver.com>
2025-08-11 14:56:27 +0900

    refact: 불필요한 상태관리 없애고 삼항연산으로 대체

--- src/components/ApplicationWork.tsx
+++ src/components/ApplicationWork.tsx
@@ -1,3 +1,27 @@
+import applicationWorkStyle from "../css/components/applicationWork.module.css";
+import ApplicationWorkItem from "./ApplicationWorkItem";
+import StateIcon from "./StateIcon";
+
 export default function ApplicationWork() {
-  return <div>지원중 의뢰</div>;
+  return (
+    <div className={applicationWorkStyle.container}>
+      <div className={applicationWorkStyle.headerDiv}>
+        <span>지원중인 의뢰가 3건 있어요! 수락되면 바로 알림보내드릴게요</span>
+      </div>
+      <div className={applicationWorkStyle.body}>
+        <ApplicationWorkItem
+          title="요아정 배달 업체 등록 사진 촬영 외주"
+          price="500,000"
+        />
+        <ApplicationWorkItem
+          title="요아정 배달 업체 등록 사진 촬영 외주"
+          price="500,000"
+        />
+        <ApplicationWorkItem
+          title="요아정 배달 업체 등록 사진 촬영 외주"
+          price="500,000"
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/StateIcon.tsx
+++ src/components/StateIcon.tsx
@@ -2,26 +2,28 @@ import progressImg from "../assets/progress-icon.png";
 import completeImg from "../assets/complete-icon.png";
 import applicationImg from "../assets/application-icon.png";
 import stateIconStyle from "../css/components/stateIcon.module.css";
-import { useEffect, useState } from "react";
 
 type Props = {
   state: string;
 };
 
 export default function StateIcon({ state }: Props) {
-  const [img, setImg] = useState(progressImg);
-  useEffect(() => {
-    if (state === "신청중") {
-      setImg(applicationImg);
-    } else if (state === "완료됨") {
-      setImg(completeImg);
-    } else {
-      setImg(progressImg);
-    }
-  }, [state]);
+  const img =
+    state === "신청중"
+      ? applicationImg
+      : state === "완료됨"
+      ? completeImg
+      : progressImg;
+
+  const bgColor =
+    state === "신청중" ? "#A7A7A7" : state === "완료됨" ? "#95BBFF" : "#5290FF";
+
   return (
-    <div className={stateIconStyle.checkDiv}>
-      <img src={img} />
+    <div
+      className={stateIconStyle.checkDiv}
+      style={{ backgroundColor: bgColor }}
+    >
+      <img src={img} alt={state} />
       <span>{state}</span>
     </div>
   );
