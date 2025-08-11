b469f510f2d4d9d25709e2100799dd0120078f2e
nyeonseok <aragorn2358@naver.com>
2025-08-11 15:37:40 +0900

    feat: 예약 중인 일칸 UI 구현

--- src/components/ApplicationIlKan.tsx
+++ src/components/ApplicationIlKan.tsx
@@ -1,4 +1,5 @@
 import applicationIlKanStyle from "../css/components/applicationIlKan.module.css";
+import ApplicationIlKanItem from "./ApplicationIlKanItem";
 
 export default function ApplicationIlKan() {
   return (
@@ -6,7 +7,20 @@ export default function ApplicationIlKan() {
       <div className={applicationIlKanStyle.headerDiv}>
         <span>예약중인 일칸이 2개 있어요!</span>
       </div>
-      <div className={applicationIlKanStyle.body}></div>
+      <div className={applicationIlKanStyle.body}>
+        <ApplicationIlKanItem
+          title="대구 반월당로 번화가 빈칸"
+          address="하늘시 비구름동 주륵주륵 304호"
+          startTime="09월 02일 08시"
+          endTime="09월 03일 15시"
+        />
+        <ApplicationIlKanItem
+          title="대구 경북 경산시 청운2로 4 사진 스튜디오"
+          address="하늘시 비구름동 주륵주륵 304호"
+          startTime="09월 02일 08시"
+          endTime="09월 03일 15시"
+        />
+      </div>
     </div>
   );
 }
