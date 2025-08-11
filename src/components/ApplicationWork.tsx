959d6b70e48960551d419f98327b0ecab20e812a
nyeonseok <aragorn2358@naver.com>
2025-08-11 15:00:07 +0900

    feat: 지원중인 의뢰 UI구현

--- src/components/ApplicationWork.tsx
+++ src/components/ApplicationWork.tsx
@@ -6,9 +6,13 @@ export default function ApplicationWork() {
   return (
     <div className={applicationWorkStyle.container}>
       <div className={applicationWorkStyle.headerDiv}>
-        <span>지원중인 의뢰가 3건 있어요! 수락되면 바로 알림보내드릴게요</span>
+        <span>지원중인 의뢰가 4건 있어요! 수락되면 바로 알림보내드릴게요</span>
       </div>
       <div className={applicationWorkStyle.body}>
+        <ApplicationWorkItem
+          title="요아정 배달 업체 등록 사진 촬영 외주"
+          price="500,000"
+        />
         <ApplicationWorkItem
           title="요아정 배달 업체 등록 사진 촬영 외주"
           price="500,000"
