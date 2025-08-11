c2552ae9ed42b956a0ee301fe15c37e3c57e08e7
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-08-11 23:55:36 +0900

    feat: App.tsx 라우팅 설정 및 파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import GlobalLayout from "./layout/GlobalLayout";
 import Index from "./pages";
 import MyPage from "./pages/main/myPage";
+import Login from "./pages/login";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<GlobalLayout />}>
+      <Route path="/">
         <Route index element={<Index />} />
-        <Route path="main">
+        <Route path="login" element={<Login />} />
+        <Route path="main" element={<GlobalLayout />}>
           <Route path="myPage" element={<MyPage />} />
         </Route>
       </Route>
