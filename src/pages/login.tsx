297f8eece9afee16a033f46171faf1b7897b0ce6
nyeonseok <aragorn2358@naver.com>
2025-08-12 10:39:25 +0900

    feat: login 간략한 UI 및 api 구현, 테스트 필요

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,62 @@
+import { FormEvent, useState } from "react";
+import loginStyle from "../css/pages/login.module.css";
+
 export default function Login() {
-  return <div>로그인</div>;
+  const [role, setRole] = useState("undefined");
+  const [errorMessage, setErrorMessage] = useState("");
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(BASE_URL + "/auth/login", {
+        method: "POST",
+        headers: {
+          "X-Role": role,
+        },
+        // credentials: "include",   // 아마 안필요할듯??
+      });
+
+      if (response.ok) {
+        const data = await response.json(); // json으로 받는지 확인 필요함
+        // 이후에 백에서 받아온 값들 뭔지 얘기 후 코드 작성
+      } else {
+        const error = await response.json();
+        setErrorMessage(error.message);
+      }
+    } catch (error: any) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  const roles = [
+    { key: "PERFORMER", label: "수행자" },
+    { key: "REQUESTER", label: "의뢰자" },
+    { key: "OWNER", label: "건물주" },
+  ];
+
+  return (
+    <form onSubmit={handleSubmit} className={loginStyle.loginForm}>
+      <div className={loginStyle.loginContainer}>
+        {roles.map(({ key, label }) => (
+          <div
+            key={key}
+            className={`${loginStyle.userItem} ${
+              role === key ? loginStyle.userItemSelected : ""
+            }`}
+            onClick={() => {
+              setRole(key);
+              console.log(key);
+            }}
+          >
+            {label}
+          </div>
+        ))}
+      </div>
+
+      {/* 가능한 오류: 서버 연결 실패, userRole 선택 안하고 로그인 정도? */}
+      {errorMessage && <span className={loginStyle.error}>{errorMessage}</span>}
+      <button type="submit">로그인</button>
+    </form>
+  );
 }
