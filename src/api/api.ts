e056e7f571ff0dc559d1dc0f77dcf79bb13bba9f
nyeonseok <aragorn2358@naver.com>
2025-08-12 15:43:03 +0900

    feat: axios설치 및 인터셉터 구현

---
+++ src/api/api.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useStore } from "../store/sotre";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  //   withCredentials: true, // httpOnly설정했는지 확인하기
+});
+
+// 요청 인터셉터
+api.interceptors.request.use(
+  (config) => {
+    const role = localStorage.getItem("role");
+    if (role) {
+      config.headers["X-Role"] = role;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// 응답 인터셉터
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (
+      error.response &&
+      (error.response.status === 401 || error.response.status === 403)
+    ) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
