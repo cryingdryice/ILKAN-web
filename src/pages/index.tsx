59f047981d394bae95188b91f06ab84ea354ab53
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-08-17 16:56:44 +0900

    feat: 홈화면 애니메이션 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,43 @@
 import { Link } from "react-router-dom";
-import indexVideo from "../assets/index.mp4"; // 동영상 파일 import
-import indexStyle from "../css/style.module.css"; // CSS 모듈 import
+import indexVideo from "../assets/index.mp4";
+import indexStyle from "../css/style.module.css";
+import { useEffect, useState } from "react";
 
 export default function Index() {
+  const [shouldRenderLink, setShouldRenderLink] = useState(false);
+  const [animateLink, setAnimateLink] = useState(false);
+
+  useEffect(() => {
+    const renderTimer = setTimeout(() => {
+      setShouldRenderLink(true);
+
+      const animateTimer = setTimeout(() => {
+        setAnimateLink(true);
+      }, 50);
+
+      return () => clearTimeout(animateTimer);
+    }, 7000);
+    return () => clearTimeout(renderTimer);
+  }, []);
+
   return (
     <div className={indexStyle.videoContainer}>
       <video className={indexStyle.video} autoPlay muted playsInline>
         <source src={indexVideo} type="video/mp4" />
       </video>
-      <div className={indexStyle.content}>
-        <Link to="/login" className={indexStyle.startButton}>
-          시작하기
-        </Link>
-      </div>
+
+      {shouldRenderLink && (
+        <div className={indexStyle.content}>
+          <Link
+            to="/login"
+            className={`${indexStyle.startButton} ${
+              animateLink ? indexStyle.show : ""
+            }`}
+          >
+            시작하기
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
