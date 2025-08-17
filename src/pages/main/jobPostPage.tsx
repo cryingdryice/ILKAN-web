5ef106d96996f8975ae50479bb65919e5c9598a8
Jiwon Park <parkjiwon0821@naver.com>
2025-08-17 14:09:22 +0900

    feat: 버튼 추가

--- src/pages/main/jobPostPage.tsx
+++ src/pages/main/jobPostPage.tsx
@@ -3,7 +3,12 @@ import jobPostPageStyle from "../../css/pages/jobPostPage.module.css";
 export default function JobPostPage() {
   return (
     <div className={jobPostPageStyle.jobPostPageContainer}>
-      <header>전문가 모집 공고 쓰기</header>
+      <header className={jobPostPageStyle.header}>전문가 모집 공고 쓰기</header>
+      <div>공고제목</div>
+      <div>세부사항</div>
+      <div>모집조건</div>
+      <div>상세조건</div>
+      <button className={jobPostPageStyle.postBtn}>공고 올리기</button>
     </div>
   );
 }
