e1acd14cba92b0a026a08de57f981d80cbe99932
Jiwon Park <parkjiwon0821@naver.com>
2025-08-17 16:31:44 +0900

    feat: 커스텀드롭다운 추가 구현

--- src/components/jobPost/CategoryDropdown.tsx
+++ src/components/jobPost/CategoryDropdown.tsx
@@ -58,16 +58,18 @@ export default function CategoryDropdown({
         </div>
         <svg
           className={styles.chevron}
-          width="18"
-          height="18"
-          viewBox="0 0 24 24"
-          aria-hidden
+          width="19"
+          height="10"
+          viewBox="0 0 19 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M6 9l6 6 6-6"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
+            d="M1 9.33334L9.33334 1L17.6667 9.33334"
+            stroke="#092F63"
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
           />
         </svg>
       </button>
