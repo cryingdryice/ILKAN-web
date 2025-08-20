fd45a86a1f5df67beae0878a597bffbb049ac246
nyeonseok <aragorn2358@naver.com>
2025-08-20 16:13:13 +0900

    feat: 사진이랑 프롬프트 모두 입력해야 폼 버튼 활성화 되도록

--- src/pages/main/remodelingIlKanPage.tsx
+++ src/pages/main/remodelingIlKanPage.tsx
@@ -3,14 +3,17 @@ import addImgLabel from "../../assets/remodelingIlKan/addImg.svg";
 import promptLabel from "../../assets/remodelingIlKan/prompt.svg";
 import aiResultLabel from "../../assets/remodelingIlKan/aiResult.svg";
 import addImg from "../../assets/remodelingIlKan/addIlKanImg.svg";
-import { useRef, useState, ChangeEvent } from "react";
+import formBtn from "../../assets/remodelingIlKan/formBtn.svg";
+import { useRef, useState, ChangeEvent, FormEvent } from "react";
+import api from "../../api/api";
 export default function RemodelingIlKanPage() {
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const [selectedImageFile, setSelectedImageFile] = useState<File | null>(null);
-  const [promptText, setPromptText] = useState<string>("");
+  const [imageName, setImageName] = useState<string>("");
+  const [prompt, setPrompt] = useState<string>("");
   const [aiResultImages, setAiResultImages] = useState<string[]>([]);
-
+  const [canSubmit, setCanSubmit] = useState<boolean>(false);
+  console.log(canSubmit);
   const handleAddImageClick = () => {
     fileInputRef.current?.click();
   };
@@ -18,11 +21,62 @@ export default function RemodelingIlKanPage() {
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
-      setSelectedImageFile(file);
+      if (prompt !== "") {
+        setCanSubmit(true);
+      } else {
+        setCanSubmit(false);
+        setImageName(file.name);
+      }
+    }
+  };
+
+  const handlePromptChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPrompt(event.target.value);
+    if (event.target.value !== "") {
+      if (imageName) {
+        setCanSubmit(true);
+      }
+    } else {
+      setCanSubmit(false);
+    }
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData();
+    if (prompt) {
+      const jsonData = JSON.stringify(prompt);
+      const blob = new Blob([jsonData], { type: "application/json" });
+      formData.append("prompt", blob); //백이랑 얘기 후  이름 맞춰야함
+    }
+    if (imageName) {
+      const imageInput = document.getElementById(
+        "imageUpload"
+      ) as HTMLInputElement;
+      const image = imageInput?.files?.[0];
+      if (image) {
+        formData.append("image", image); //백이랑 얘기 후  이름 맞춰야함
+      }
+    }
+
+    try {
+      const response = await api.post("주소주소주소", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      if (response.status === 200) {
+        alert("업로드 완료");
+      }
+    } catch (error: any) {
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        "알 수 없는 오류 발생";
     }
   };
   return (
-    <form className={remodelingIlKanStyle.container}>
+    <form className={remodelingIlKanStyle.container} onSubmit={handleSubmit}>
       <div className={remodelingIlKanStyle.headerDiv}>
         생성형 AI로 보는 공실의 가능성
       </div>
@@ -38,6 +92,7 @@ export default function RemodelingIlKanPage() {
           <img src={addImg} alt="빈 공실 사진 추가하기" />
           <span>빈 공실 사진 추가하기</span>
           <input
+            id="imageUpload"
             type="file"
             ref={fileInputRef}
             onChange={handleFileChange}
@@ -54,6 +109,7 @@ export default function RemodelingIlKanPage() {
         <input
           type="text"
           placeholder="공실의 미래를 생각하며 작성해주세요"
+          onChange={handlePromptChange}
           required
         />
       </div>
@@ -68,6 +124,14 @@ export default function RemodelingIlKanPage() {
           <div className={remodelingIlKanStyle.resultItem}></div>
         </div>
       </div>
+      <button
+        className={remodelingIlKanStyle.formBtn}
+        disabled={!canSubmit}
+        type="submit"
+      >
+        <img src={formBtn} alt="더보기" />
+        <span>더보기</span>
+      </button>
     </form>
   );
 }
