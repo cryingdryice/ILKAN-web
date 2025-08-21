171a5a9c9f02b7794b91a3ab4b0f8b2df4834269
chanju <ckswn1244@gmail.com>
2025-08-22 02:12:08 +0900

    feat: footer 수정 및 지원 확인 창 레이아웃 제작

--- src/pages/main/jobsSuccessPage.tsx
+++ src/pages/main/jobsSuccessPage.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"; // ✅ useEffect, useState 추가
-import { useParams } from "react-router-dom"; // ✅ useParams 추가
+import { useParams, Link } from "react-router-dom"; // ✅ useParams 추가
 import styles from "../../css/pages/jobsSuccessPage.module.css";
 import SuccessLogo from "../../assets/success.svg";
 import api from "../../api/api"; // ✅ api 불러오기
+import Reserving from "../../assets/reserving.svg";
+
 interface DetailInfo {
   taskId: number;
   title: string;
@@ -44,7 +46,17 @@ export default function JobsSuccessPage() {
       ></img>
 
       <label className={styles.labelSize}>예약이 완료되었습니다!</label>
-      <div className={styles.reservationBox}>{detailInfo.title}</div>
+      <div className={styles.titleBox}>
+        <img src={Reserving} className={styles.condition}></img>
+        <span className={styles.writer}>
+          {detailInfo.title}
+          {/* 사업자명 api 값 들어오면 수정해야합니다 */}
+        </span>
+        <label className={styles.title}>{detailInfo.title}</label>
+      </div>
+      <Link to="/main/myPage" className={styles.mypageBtn}>
+        MYPAGE에서 확인하기 &gt;
+      </Link>
     </div>
   );
 }
