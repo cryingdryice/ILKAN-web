589b7cadd33ab5ae64dd7017c298dc360c456fc9
Jiwon Park <parkjiwon0821@naver.com>
2025-08-23 05:19:17 +0900

    feat: 필드2 UI 구현

--- src/components/kanPost/KanField2.tsx
+++ src/components/kanPost/KanField2.tsx
@@ -1,3 +1,5 @@
+// src/components/kanPost/KanField2.tsx
+import { useState } from "react";
 import kanFieldStyle from "../../css/components/kanPost/kanField2.module.css";
 import newDocumentIcon from "../../assets/jobPost/new-document-icon.svg";
 import termIcon from "../../assets/jobPost/term-icon.svg";
@@ -5,7 +7,21 @@ import rewardIcon from "../../assets/jobPost/reward-icon.svg";
 import emailIcon from "../../assets/jobPost/email-icon.svg";
 import telephoneIcon from "../../assets/jobPost/telephone-icon.svg";
 
+import TimePicker, { TimeValue, to24hString } from "../kanPost/TimePicker";
+
 export default function KanField2() {
+  // ✅ 시간 상태
+  const [checkIn, setCheckIn] = useState<TimeValue>({
+    ampm: "오전",
+    hour: 8,
+    minute: 0,
+  });
+  const [checkOut, setCheckOut] = useState<TimeValue>({
+    ampm: "오전",
+    hour: 8,
+    minute: 0,
+  });
+
   return (
     <section className={kanFieldStyle.postFieldContainer}>
       <div className={kanFieldStyle.fieldTitle}>
@@ -16,20 +32,17 @@ export default function KanField2() {
         />
         세부 사항
       </div>
+
       <div className={kanFieldStyle.fieldGrid}>
         <div className={kanFieldStyle.fieldItem}>
-          <img
-            src={termIcon}
-            alt="new document"
-            className={kanFieldStyle.icon}
-          />
+          <img src={termIcon} alt="" className={kanFieldStyle.icon} />
           <div className={kanFieldStyle.textBox}>
-            <label className={kanFieldStyle.itemTitle} htmlFor="period">
+            <label className={kanFieldStyle.itemTitle} htmlFor="type">
               작업실 유형
             </label>
             <input
-              id="period"
-              name="period"
+              id="type"
+              name="type"
               type="text"
               className={kanFieldStyle.itemDesc}
               required
@@ -39,11 +52,7 @@ export default function KanField2() {
         </div>
 
         <div className={kanFieldStyle.fieldItem}>
-          <img
-            src={rewardIcon}
-            alt="new document"
-            className={kanFieldStyle.icon}
-          />
+          <img src={rewardIcon} alt="" className={kanFieldStyle.icon} />
           <div className={kanFieldStyle.textBox}>
             <label className={kanFieldStyle.itemTitle} htmlFor="pay">
               대여비 (하루)
@@ -60,11 +69,7 @@ export default function KanField2() {
         </div>
 
         <div className={kanFieldStyle.fieldItem}>
-          <img
-            src={emailIcon}
-            alt="new document"
-            className={kanFieldStyle.icon}
-          />
+          <img src={emailIcon} alt="" className={kanFieldStyle.icon} />
           <div className={kanFieldStyle.textBox}>
             <label className={kanFieldStyle.itemTitle} htmlFor="email">
               이메일
@@ -81,11 +86,7 @@ export default function KanField2() {
         </div>
 
         <div className={kanFieldStyle.fieldItem}>
-          <img
-            src={telephoneIcon}
-            alt="new document"
-            className={kanFieldStyle.icon}
-          />
+          <img src={telephoneIcon} alt="" className={kanFieldStyle.icon} />
           <div className={kanFieldStyle.textBox}>
             <label className={kanFieldStyle.itemTitle} htmlFor="phone">
               전화
@@ -97,6 +98,35 @@ export default function KanField2() {
               className={kanFieldStyle.itemDesc}
               placeholder="전화번호를 입력해주세요"
               required
+            />
+          </div>
+        </div>
+
+        {/* ✅ 입실 시간 */}
+        <div className={kanFieldStyle.fieldItem}>
+          <img src={rewardIcon} alt="" className={kanFieldStyle.icon} />
+          <div className={kanFieldStyle.textBox}>
+            <div className={kanFieldStyle.itemTitle}>입실 시간</div>
+            <div className={kanFieldStyle.itemDesc}>
+              <TimePicker value={checkIn} onChange={setCheckIn} />
+            </div>
+            {/* 폼 전송용 24h hidden */}
+            <input type="hidden" name="checkIn" value={to24hString(checkIn)} />
+          </div>
+        </div>
+
+        {/* ✅ 퇴실 시간 */}
+        <div className={kanFieldStyle.fieldItem}>
+          <img src={rewardIcon} alt="" className={kanFieldStyle.icon} />
+          <div className={kanFieldStyle.textBox}>
+            <div className={kanFieldStyle.itemTitle}>퇴실 시간</div>
+            <div className={kanFieldStyle.itemDesc}>
+              <TimePicker value={checkOut} onChange={setCheckOut} />
+            </div>
+            <input
+              type="hidden"
+              name="checkOut"
+              value={to24hString(checkOut)}
             />
           </div>
         </div>
