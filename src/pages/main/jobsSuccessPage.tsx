1ba37716156781f84c405fa8423edf4260c9ded2
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-08-24 03:52:58 +0900

    feat: jobsSuccesspage 모달 추가

--- src/pages/main/jobsSuccessPage.tsx
+++ src/pages/main/jobsSuccessPage.tsx
@@ -4,6 +4,8 @@ import styles from "../../css/pages/jobsSuccessPage.module.css";
 import SuccessLogo from "../../assets/success.svg";
 import api from "../../api/api"; // ✅ api 불러오기
 import Reserving from "../../assets/reserving.svg";
+import Modal from "../../components/Modal";
+import modalStyle from "../../css/components/modal.module.css";
 
 interface DetailInfo {
   taskId: number;
@@ -12,6 +14,12 @@ interface DetailInfo {
 export default function JobsSuccessPage() {
   const { id } = useParams<{ id: string }>();
   const [detailInfo, setDetailInfo] = useState<DetailInfo | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalText, setModalText] = useState("");
+  const [modalTitle, setModalTitle] = useState("");
+  const [modalOnConfirm, setModalOnConfirm] = useState<(() => void) | null>(
+    null
+  );
 
   const fetchDetailInfo = async () => {
     console.log(id);
@@ -21,14 +29,20 @@ export default function JobsSuccessPage() {
         setDetailInfo(response.data);
       } else {
         const error = await response.data;
-        alert(error.message);
+        // alert(error.message);
+        setModalTitle("지원중인 의뢰");
+        setModalText(error.message);
+        setIsOpen(true);
       }
     } catch (error: any) {
       const errorMessage =
         error.response?.data?.message ||
         error.message ||
         "알 수 없는 오류 발생";
-      alert(errorMessage);
+      // alert(errorMessage);
+      setModalTitle("지원중인 의뢰");
+      setModalText(errorMessage);
+      setIsOpen(true);
     }
   };
   useEffect(() => {
@@ -39,6 +53,16 @@ export default function JobsSuccessPage() {
   }
   return (
     <div className={styles.wrapper}>
+      {isOpen && (
+        <div className={modalStyle.overlay}>
+          <Modal
+            setIsOpen={setIsOpen}
+            text={modalText}
+            title={modalTitle}
+            onConfirm={modalOnConfirm || undefined}
+          />
+        </div>
+      )}
       <img
         src={SuccessLogo}
         className={styles.logoSize}
