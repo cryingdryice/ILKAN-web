51cdede2af316d0adeb3746840064429ebe089bf
Jiwon Park <parkjiwon0821@naver.com>
2025-08-24 06:52:25 +0900

    feat: 버튼 연결

--- src/components/myPage/AddMyIlKanBtn.tsx
+++ src/components/myPage/AddMyIlKanBtn.tsx
@@ -1,10 +1,15 @@
 import registeredIlKanStyle from "../../css/components/myPage/registedIlKan.module.css";
 import addIcon from "../../assets/myPage/addIlKan.svg";
+import { Link } from "react-router-dom";
 export default function AddMyIlKanBtn() {
   return (
-    <div className={registeredIlKanStyle.addDiv}>
-      <img src={addIcon} alt="일칸 추가" />
+    <Link
+      to="/main/kanPost"
+      className={registeredIlKanStyle.addDiv}
+      aria-label="나의 공실 추가하기"
+    >
+      <img src={addIcon} alt="" aria-hidden="true" />
       <span>나의 공실 추가하기</span>
-    </div>
+    </Link>
   );
 }
