45a38d4d53eef83562171c530d6b273562167881
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-08-24 01:10:36 +0900

    feat: 모달 추가

---
+++ src/components/Modal.tsx
@@ -0,0 +1,36 @@
+import modalStyle from "../css/components/modal.module.css";
+import cancelImg from "../assets/myPage/X.svg";
+
+type Props = {
+  setIsOpen: (isOpen: boolean) => void;
+  text: string;
+  title: string;
+  // btnColor: string;
+};
+
+export default function Modal({ setIsOpen, text, title }: Props) {
+  return (
+    <div className={modalStyle.modalContainer}>
+      <div className={modalStyle.modalHeader}>
+        <span className={modalStyle.headerText}>{title}</span>
+        <button
+          className={modalStyle.closeBtn}
+          onClick={() => setIsOpen(false)}
+        >
+          <img src={cancelImg} alt="close" onClick={() => setIsOpen(false)} />
+        </button>
+      </div>
+      <div className={modalStyle.modalContent}>
+        <span className={modalStyle.contentText}>{text}</span>
+      </div>
+      <div className={modalStyle.modalBtnDiv}>
+        <button
+          className={modalStyle.modalBtn}
+          onClick={() => setIsOpen(false)}
+        >
+          확인
+        </button>
+      </div>
+    </div>
+  );
+}
