4cfcf29a9f7daf1bdf54a3eee245a5a7b81e8715
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-08-24 17:49:03 +0900

    feat: 수행자 빌리고 있는 일칸 조회

--- src/components/myPage/ProgressingIlKan.tsx
+++ src/components/myPage/ProgressingIlKan.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import progressingIlKanStyle from "../../css/components/myPage/progressingIlKan.module.css";
 import StateIcon from "../StateIcon";
 import inIcon from "../../assets/myPage/In-icon.svg";
@@ -30,54 +30,33 @@ export default function ProgressingIlKan({ role }: Props) {
     null
   );
 
-  const mockItems: Items[] = [
-    {
-      reservationId: 201,
-      buildingId: 301,
-      buildingName: "경산시 조영동 사진 스튜디오 일칸",
-      buildingAddress: "하늘시 비구름동 주륵주륵 304호",
-      startTime: "2025-07-17T13:40:22.276Z",
-      endTime: "2025-08-30T13:40:22.276Z",
-      buildingImage: "https://example.com/images/building.jpg",
-    },
-    {
-      reservationId: 202,
-      buildingId: 302,
-      buildingName: "서울 강남구 프리랜서 작업실",
-      buildingAddress: "서울특별시 강남구 테헤란로 123",
-      startTime: "2025-07-17T13:40:22.276Z",
-      endTime: "2025-09-17T13:40:22.276Z",
-      buildingImage: "https://example.com/images/building.jpg",
-    },
-  ];
-  const dataToRender = items.length > 0 ? items : mockItems;
-  // const fetchWorkInfo = async () => {
-  //   try {
-  //     const response = await api.get("/myprofile/buildings/using");
-  //     if (response.status === 200) {
-  //       setItems(response.data);
-  //     } else {
-  //       const error = await response.data;
-  //       // alert(error.message);
-  //       setModalTitle("빌리고 있는 일칸");
-  //       setModalText(error.message);
-  //       setIsOpen(true);
-  //     }
-  //   } catch (error: any) {
-  //     const errorMessage =
-  //       error.response?.data?.message ||
-  //       error.message ||
-  //       "알 수 없는 오류 발생";
-  //     // alert(errorMessage);
-  //     setModalTitle("빌리고 있는 일칸");
-  //     setModalText(errorMessage);
-  //     setIsOpen(true);
-  //   }
-  // };
+  const fetchWorkInfo = async () => {
+    try {
+      const response = await api.get("/myprofile/buildings/using");
+      if (response.status === 200) {
+        setItems(response.data.content);
+      } else {
+        const error = await response.data;
+        // alert(error.message);
+        setModalTitle("빌리고 있는 일칸");
+        setModalText(error.message);
+        setIsOpen(true);
+      }
+    } catch (error: any) {
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        "알 수 없는 오류 발생";
+      // alert(errorMessage);
+      setModalTitle("빌리고 있는 일칸");
+      setModalText(errorMessage);
+      setIsOpen(true);
+    }
+  };
 
-  // useEffect(() => {
-  //   fetchWorkInfo();
-  // }, []);
+  useEffect(() => {
+    fetchWorkInfo();
+  }, []);
   return (
     <div className={progressingIlKanStyle.container}>
       {isOpen && (
@@ -93,17 +72,22 @@ export default function ProgressingIlKan({ role }: Props) {
       <div className={progressingIlKanStyle.headerDiv}>
         <StateIcon state="진행중" evaluation={false} />
         <span className={progressingIlKanStyle.headerTitle}>
-          지금 빌리고 있는 일칸이 {mockItems.length}개 있어요!
+          지금 빌리고 있는 일칸이 {items.length}개 있어요!
         </span>
       </div>
       <div className={progressingIlKanStyle.body}>
-        {dataToRender.map((item) => (
+        {items.map((item) => (
           <div
             key={item.reservationId}
             className={progressingIlKanStyle.itemContainer}
           >
             <div className={progressingIlKanStyle.itemHeader}>
-              <div className={progressingIlKanStyle.itemImgDiv}></div>
+              <div className={progressingIlKanStyle.itemImgDiv}>
+                <img
+                  src={`https://ilkan-image.s3.ap-northeast-2.amazonaws.com/${item.buildingImage}`}
+                  alt="사진"
+                ></img>
+              </div>
               <div className={progressingIlKanStyle.itemRightDiv}>
                 <div className={progressingIlKanStyle.itemTitle}>
                   <span>{item.buildingName}</span>
@@ -131,7 +115,10 @@ export default function ProgressingIlKan({ role }: Props) {
               />
             </div>
             <div className={progressingIlKanStyle.footer}>
-              <a href="#" className={progressingIlKanStyle.viewLink}>
+              <a
+                href={`/main/kanMatch/${item.buildingId}`}
+                className={progressingIlKanStyle.viewLink}
+              >
                 공고 보러가기{" >"}
               </a>
             </div>
